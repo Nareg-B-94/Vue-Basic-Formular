aafd3c0c40f4661f4f019bb3390df5d94875518e
Nareg Berberian <101928928+Nareg-B-94@users.noreply.github.com>
2023-03-14 09:45:36 +0100

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>test</h1>
+  <SignupForm />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignupForm from './components/SignupForm.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { SignupForm },
+};
 </script>
 
 <style>
@@ -22,5 +20,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #818181;
 }
 </style>
